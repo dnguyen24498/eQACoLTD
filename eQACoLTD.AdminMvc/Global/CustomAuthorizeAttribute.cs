bb1712e20cfa005ae4dd91ae7a31091fdea8836b
dnguyen24498 <62144517+dnguyen24498@users.noreply.github.com>
2020-09-11 15:56:55 +0700

    - BIG UPDATE: Restructure All Project

---
+++ eQACoLTD.AdminMvc/Global/CustomAuthorizeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace eQACoLTD.AdminMvc.Global
+{
+    public class CustomAttribute:AuthorizeAttribute,IAuthorizationFilter 
+    {
+        public string Permissions { get; set; }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (string.IsNullOrEmpty(Permissions))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            var userRoles = context.HttpContext.User.Claims.Where(x => x.Type == "role").SingleOrDefault()?.Value;
+            var requirementRoles = Permissions.Split(",");
+            bool isContain = false;
+            foreach (var r in requirementRoles)
+            {
+                if (userRoles.Contains(r)) isContain = true;
+            }
+
+            if (!isContain)
+            {
+                context.Result=new UnauthorizedResult();
+                return;
+            }
+            return;
+        }
+    }
+}
